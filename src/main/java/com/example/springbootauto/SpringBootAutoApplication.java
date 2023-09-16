256be6bee321b38e087f2637acdfcbd287095165
wangjianDiao <756862057@qq.com>
2023-09-16 13:04:49 +0800

    add

---
+++ src/main/java/com/example/springbootauto/SpringBootAutoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutoApplication.class, args);
+	}
+
+}
